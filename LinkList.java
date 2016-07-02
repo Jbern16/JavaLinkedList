b90a6419085f3dba7294887bccf3bfc9aec4caea
Jbern16 <jbern16@gmail.com>
2016-07-02 14:10:54 -0600

    add update, delete, pop, find, find parent

--- LinkList.java
+++ LinkList.java
@@ -6,25 +6,92 @@ public class LinkList {
   }
   public void insert(String data) {
     if (this.head.next == null) {
-      this.head.next = new Node(data);
+        this.head.next = new Node(data);
     } else {
-      insert(data, this.head);
+        insert(data, this.head);
     }
   } 
   private void insert(String data, Node currentNode) {
     if (currentNode.next == null) {
-      currentNode.next = new Node(data);
+        currentNode.next = new Node(data);
     } else {
-      insert(data, currentNode.next);
+        insert(data, currentNode.next);
     }
   }
-  
 
+  public Node pop() {
+    Node tail = tail();
+    findParent(tail.data).next = null;
+    return tail;
+  }
 
+  public Node tail() {
+    if (this.head.next == null) {
+        return this.head;
+    } else {
+        return tail(this.head.next);
+    }
+  }
 
+  private Node tail(Node currentNode) {
+    if (currentNode.next == null) {
+        return currentNode;
+    } else {
+        return tail(currentNode.next);
+    }
+  }
 
+  public Boolean update(String toUpdate, String newData){
+    Node node = find(toUpdate);
+    if (node != null) {
+        node.data = newData;
+        return true;
+    } else {
+        return false;
+    }
+  }
 
+  public Node find (String data) {
+    if (this.head.data == data) {
+        return this.head;
+    } else {
+        return find(data, this.head.next);
+    }
+  }
 
+  private Node find (String data, Node currentNode) {
+    if (currentNode.data == data) {
+        return currentNode;
+    } else {
+        return find(data, currentNode.next);
+    }
+  }
+
+  public Node findParent(String data) {
+    if (this.head.data == data) {
+        return null;
+    } else {
+        return findParent(data, this.head);
+    }
+  }
+
+  private Node findParent(String data, Node currentNode) {
+    if (currentNode.next.data == data ) {
+        return  currentNode; 
+    } else  { 
+        return findParent(data, currentNode.next);
+    }
+  }
+
+  public Boolean delete(String data) {
+    Node node = find(data);
+    if (node != null) {
+      findParent(node.data).next = node.next;
+      return true; 
+    } else  {
+      return false;
+    }
+  }
 
   public void print() {
     System.out.println(this.head.data);
@@ -49,6 +116,10 @@ public class LinkList {
     list.insert("node 2");
     list.insert("node 3");
 
+    System.out.println(list.pop().data);
+    
     list.print();
+
+    
   }
 }
